b43ad596c678614d79d4e022d01ef61f641a226e
Alexandra2406 <92298360+Alexandra2406@users.noreply.github.com>
2022-06-27 00:00:32 +0300

    Add files via upload

---
+++ lab6/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab6Var10
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public Node Next { get; set; }
+
+        public Node(string value, Node next)
+        {
+            Value = value;
+            Next = next;
+        }
+    }
+}
